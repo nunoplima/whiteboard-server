187e0b4305b9a9e0eba8560451cb5cc13ce89177
nuno <ngplbc@gmail.com>
2020-03-25 20:04:49 +0000

    uncomment result methods

--- models/Result.js
+++ models/Result.js
@@ -28,28 +28,28 @@ Result.findAll = (cb) => {
     });
 };
 
-// Result.find = (wodId, userId, cb) => {
-//     const sql = `SELECT user_id, username, wod_id, result, result.created_at, updated_at FROM result 
-//         JOIN user
-//             ON user.id = result.user_id
-//         WHERE wod_id = ? AND user_id = ?`;
-//     connection.query(sql, [wodId, userId], (err, results, fields) => {
-//         cb(err, results);
-//     });
-// };
+Result.find = (wodId, userId, cb) => {
+    const sql = `SELECT user_id, username, wod_id, result, result.created_at, updated_at FROM result 
+        JOIN user
+            ON user.id = result.user_id
+        WHERE wod_id = ? AND user_id = ?`;
+    connection.query(sql, [wodId, userId], (err, results, fields) => {
+        cb(err, results);
+    });
+};
 
-// Result.create = (resultData, cb) => {
-//     const sql = `INSERT INTO result SET ?`
-//     connection.query(sql, [resultData], (err, results, fields) => {
-//         cb(err, results);
-//     });
-// };
+Result.create = (resultData, cb) => {
+    const sql = `INSERT INTO result SET ?`
+    connection.query(sql, [resultData], (err, results, fields) => {
+        cb(err, results);
+    });
+};
 
-// Result.update = (result, wodId, userId, cb) => {
-//     const sql = `UPDATE result SET result = ? WHERE wod_id = ? AND user_id = ?`;
-//     connection.query(sql, [result, wodId, userId], (err, results, fields) => {
-//         cb(err, results);
-//     });
-// }
+Result.update = (result, wodId, userId, cb) => {
+    const sql = `UPDATE result SET result = ? WHERE wod_id = ? AND user_id = ?`;
+    connection.query(sql, [result, wodId, userId], (err, results, fields) => {
+        cb(err, results);
+    });
+}
 
 module.exports = Result;
