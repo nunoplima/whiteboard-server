c674082e2be9daaa7d4725788106f22035825079
nuno <ngplbc@gmail.com>
2020-04-03 17:37:27 +0100

    get wods w box details

--- models/Wod.js
+++ models/Wod.js
@@ -3,7 +3,11 @@ const connection = require("../db/config");
 const Wod = {};
 
 Wod.findAll = (cb) => {
-    const sql = `SELECT * FROM wod 
+    const sql = `SELECT 
+        wod.id, wod.box_id, box.name AS box_name, box.logo_url, box.box_url, description, scheduled_date, video_url, score_type, wod.created_at  
+        FROM wod 
+        LEFT JOIN box
+            ON wod.box_id = box.id
         WHERE DATE(scheduled_date) <= CURDATE()
         ORDER BY scheduled_date DESC`;
     connection.query(sql, (err, results, fields) => {
